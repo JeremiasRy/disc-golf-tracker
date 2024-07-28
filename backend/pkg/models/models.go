05b895e479a6a4f6d545971d939601ffd186025d
Jeremias Ryttäri <ryttari.jeremias@gmail.com>
2024-07-28 12:47:42 +0300

    rework models

---
+++ backend/pkg/models/models.go
@@ -0,0 +1,45 @@
+package models
+
+import "gorm.io/gorm"
+
+type Course struct {
+	gorm.Model
+	Name   string  `json:"name"`
+	Holes  []Hole  `gorm:"foreignKey:CourseID" json:"holes"`
+	Rounds []Round `gorm:"foreignKey:CourseID" json:"rounds"`
+}
+
+type Hole struct {
+	gorm.Model
+	Par      uint    `json:"par"`
+	NthHole  uint    `json:"nth_hole"`
+	CourseID uint    `json:"course_id"`
+	Scores   []Score `gorm:"foreignKey:HoleID" json:"scores"`
+}
+
+type Round struct {
+	gorm.Model
+	CourseID   uint
+	ScoreCards []ScoreCard `gorm:"foreignKey:RoundID" json:"cards"`
+}
+
+type ScoreCard struct {
+	gorm.Model
+	RoundID uint
+	UserID  uint
+	Scores  []Score `gorm:"foreignKey:ScorecardID" json:"scores"`
+}
+
+type Score struct {
+	gorm.Model
+	HoleID      uint `json:"hole_id"`
+	ScorecardID uint `json:"scorecard_id"`
+	Strokes     uint `json:"strokes"`
+	Penalties   uint `json:"penalties"`
+}
+
+type User struct {
+	gorm.Model
+	Name       string      `json:"name"`
+	ScoreCards []ScoreCard `gorm:"foreignKey:UserID" json:"score_card"`
+}
